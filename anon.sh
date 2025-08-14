b99564f8fe022bf7ae7447721b0c50cd5951d7bf
HVDES-808 <jkiprutto63003@anu.ac.ke>
2025-08-14 17:24:45 +0300

    some fresh adjustments

--- anon.sh
+++ anon.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+echo "Welcome to Kickstarter"
 sudo pacman -Syu
-sudo pacman -S tor nyx torbrowser-launcher
+sudo pacman -S tor nyx torbrowser-launcher torsocks
 sudo systemctl start tor
 sudo systemctl enable --now tor.service
 systemctl status tor.service
+ss -nlt
+wget -qO - https://api.ipfy.org; echo "Your IP address is:"
+torsocks wget -qO - https://api.ipfy.org; echo "Your IP address is:"
+source torsocks on
+wget -qO - https://api.ipfy.org; echo "Check if you are using Tor"
+echo ". torsocks on" >> ~/.bashrc
+sudo pacman -S gnu-netcat
+echo -e 'PROTOCOLINFO\r\n' | nc 127.0.0.1 9051
